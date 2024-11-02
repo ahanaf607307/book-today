1a8892f6f6d89c29fb0aa62e5565a521e941e909
Ahanaf Mubasshir <123632951+ahanaf607307@users.noreply.github.com>
2024-11-02 13:16:45 +0600

    update page to red section

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import { getStoredList } from '../Utility/addToDb';
 import Book from '../Books/Book';
+import { getWishList } from '../Utility/addWish';
 
 
 function ListedBooks() {
     const loadAllBooks = useLoaderData()
     const [books , setBooks] = useState([])
+    const [wishs , setWishes] = useState([])
     console.log(loadAllBooks);
     useEffect(() => {
       const storedList =   getStoredList()
@@ -14,6 +16,13 @@ function ListedBooks() {
      console.log(storedListInt);
      const readListBook = loadAllBooks.filter(book => storedListInt.includes(book.bookId))
      setBooks(readListBook)
+    },[])
+    useEffect(() => {
+      const storedList =   getWishList()
+     const storedListInt = storedList.map(id => parseInt(id))
+     console.log(storedListInt);
+     const wishBookList = loadAllBooks.filter(wish => storedListInt.includes(wish.bookId))
+     setWishes(wishBookList)
     },[])
   return (
     <div>
@@ -36,7 +45,12 @@ function ListedBooks() {
     aria-label="Wish List"
     defaultChecked />
   <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-    Wish List
+  <h1>Wishes Added : {wishs.length}</h1>
+   <div className='grid md:grid-cols-2 gap-4'>
+   {
+        wishs.map(book => <Book key={book.bookId} book={book}></Book>)
+    }
+   </div>
   </div>
 </div>
     </div>
--- src/components/Pagetoread/Pagetoread.jsx
+++ src/components/Pagetoread/Pagetoread.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Watch } from 'react-loader-spinner'
 
 function Pagetoread() {
   return (
-    <div>Pagetoread</div>
+    <div><div className='flex flex-col gap-5 w-full mx-auto'>
+         <Watch
+  visible={true}
+  height="80"
+  width="80"
+  radius="48"
+  color="#FF4545"
+  ariaLabel="watch-loading"
+  wrapperStyle={{}}
+  wrapperClass="mx-auto mt-10"
+  />
+     <h1 className='text-5xl mt-50 text-center text-red-600 font-bold '>
+
+        Update will soon 
+        </h1> </div>
+    </div>
   )
 }
 
